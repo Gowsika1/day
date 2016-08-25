31b8f42abdab7a35aaa827861a7e0c28fb5804f2
Gowsika1 <gowsikakumaravel@gmail.com>
2016-08-25 09:31:19 +0530

    Create day1.java

---
+++ https/github.com/Gowsika1/day/tree/day1.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
+
+public class day1
+{
+
+
+    public static void main(String[] args)
+    {
+       
+    Scanner sc=new Scanner(System.in);
+    String s1="monday";
+    String s2="tuesday";
+    String s3="wednesday";
+    String s4="hrusday";
+    String s5="friday";
+    String s6="saturday";
+    String s7="sunday";
+    String days=sc.nextLine();
+   
+    if(s7.equals(days))
+    {
+        System.out.println("false");
+    }
+    else if((s1.equals(days))||(s2.equals(days))||(s3.equals(days))||(s4.equals(days))||(s5.equals(days))||(s6.equals(days)))
+    {
+        System.out.println("true");
+    }
+    else
+    {
+        System.out.println("invalid");
+    }
+   
+   }
+   
+
+}
